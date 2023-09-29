89d0dc2b97a0103febaedf13f5802780c0846299
Rokon <ratertara.rokon@gmail.com>
2023-09-30 00:10:00 +0600

    SearchForm component update

--- src/SearchForm.jsx
+++ src/SearchForm.jsx
@@ -1,5 +1,26 @@
 const SearchForm = () => {
-    return <h2>Search form</h2>;
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        const searchText = e.target.elements.search.value;
+        if (!e.target.elements.search.value) return;
+        console.log(searchText);
+    };
+    return (
+        <section>
+            <h2 className="title">Unsplash image...</h2>
+            <form className="search-form" onSubmit={handleSubmit}>
+                <input
+                    type="text"
+                    className="form-input search-input"
+                    placeholder="cat"
+                    name="search"
+                />
+                <button type="submit" className="btn">
+                    Search
+                </button>
+            </form>
+        </section>
+    );
 };
 
 export default SearchForm;
