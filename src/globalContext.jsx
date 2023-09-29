de00275e177c1ae6929395ca8863694fda9b6726
Rokon <ratertara.rokon@gmail.com>
2023-09-30 02:56:59 +0600

    `dark mode` updated with user preference

--- src/globalContext.jsx
+++ src/globalContext.jsx
@@ -1,19 +1,32 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useContext } from "react";
 import { createContext } from "react";
 
 const AppContext = createContext();
 
+const setInitialDarkMode = () => {
+    const preferDarkMode = window.matchMedia(
+        "prefers-color-scheme:dark"
+    ).matches;
+
+    const localDarkTheme = localStorage.getItem("darkTheme") === "true";
+    return localDarkTheme || preferDarkMode;
+};
+
 export const AppProvider = ({ children }) => {
-    const [isDarkTheme, setIsDarkTheme] = useState(false);
+    const [isDarkTheme, setIsDarkTheme] = useState(setInitialDarkMode);
     const [searchTerm, setSearchTerm] = useState("cat");
 
     const toggleDarkTheme = () => {
         const newDarkTheme = !isDarkTheme;
         setIsDarkTheme(newDarkTheme);
-        const body = document.querySelector("body");
-        body.classList.toggle("dark-theme", newDarkTheme);
+        localStorage.setItem("darkTheme", newDarkTheme);
     };
+
+    useEffect(() => {
+        const body = document.querySelector("body");
+        body.classList.toggle("dark-theme", isDarkTheme);
+    }, [isDarkTheme]);
 
     return (
         <AppContext.Provider
